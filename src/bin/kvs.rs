c5b248aa2912097504740d3ed66e77a4acc61e9b
drakobian <drake.bennion@everee.com>
2022-08-21 10:30:46 -0600

    implement methods for cli

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,48 @@
+use kvs::KvStore;
+
+use clap::{Parser, ValueEnum};
+
+// TODO: Validate that key is always given,
+// and that value is given for Set
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Command {
+    Get,
+    Set, 
+    Rm
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The command to run
+    #[clap(arg_enum, value_parser)]
+    cmd: Command,
+
+    /// The key to use
+    #[clap(value_parser)]
+    key: String,
+
+    /// The value to optionally use
+    #[clap(value_parser)]
+    value: Option<String>
+}
+
 fn main() {
-    println!("This is kvs");
+    let args = Args::parse();
+    //println!("{:?}", args); 
+
+    let mut kvs = KvStore::new();
+
+    match args.cmd {
+        Command::Get => { 
+            kvs.get(args.key);
+            ()
+        },
+        Command::Set => {
+            kvs.set(args.key, args.value.unwrap())
+        },
+        Command::Rm  => {
+            kvs.remove(args.key)
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::collections::HashMap;
+
+pub struct KvStore {
+    map: HashMap<String, String>
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore { map: HashMap::new() } 
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+       self.map.insert(key, value);
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        self.map.get(&key).cloned()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
     }
 }
